f1d49026b4c6cb1a08166bc414994384ae4e3622
shion <2201376@s.asojuku.ac.jp>
2024-09-27 10:02:51 +0900

    1

---
+++ PHP/admin/admin_top.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../CSS/admin/admin-top.css">
+    <title> 管理者画面 </title>
+</head>
+<body>
+
+
+    <button type="submit" class="user">
+        <a href="admin_user1.php">ユーザー管理</a>
+    </button>
+    <button type="submit" class="rebyu">
+        <a href="admin_rebyu1.php">レビュー管理</a>
+    </button>
+    <button type="button" class="pass">
+        <a href="admin_pass1.php">パスワード管理</a>
+    </button>
+
+<script src="../JavaScript/hamburger.js"></script>
+
+<style>
+html,body {
+  overflow-y: hidden;
+} 
+</style>
