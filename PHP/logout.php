6636229ec544eff654aa935ce51c690ef64e7e21
sota022 <144978935+sota022@users.noreply.github.com>
2024-12-02 11:51:33 +0900

    logout

--- PHP/logout.php
+++ PHP/logout.php
@@ -14,7 +14,7 @@ session_destroy();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログアウト</title>
-    <link rel="stylesheet" href="css/logout.css">
+    <link rel="stylesheet" href="../css/logout.css">
 </head>
 <body>
 <div class="logout-container">
