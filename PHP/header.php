0a6e34bd7efa27e0ca0f81de2e46e9d248677553
shion <2201376@s.asojuku.ac.jp>
2024-12-17 14:14:36 +0900

    ヘッツァー　V1.1

--- PHP/header.php
+++ PHP/header.php
@@ -16,33 +16,37 @@ session_start();
 </head>
 <body class="bg-gray-100">
 
-    <!-- ヘッダー -->
-
-    <header class="flex items-center justify-between bg-gray-100 shadow-md p-2 fixed top-0 left-0 w-full z-50 ">
-        <!-- ロゴ -->
-        <div class="flex items-center">
+<!-- ヘッダー -->
+<header class="flex items-center justify-between bg-gray-100 shadow-md p-2 fixed top-0 left-0 w-full z-50">
+    <!-- ロゴ -->
+    <div class="flex items-center">
+        <!-- ロゴをリンク化 -->
+        <a href="top.php" class="flex items-center">
             <img src="../IMG/うんめい.png" alt="運命の一撃ロゴ" class="mr-4 ml-1" width="100" height="50">
-            <h1 class="text-lg font-bold text-yellow-600 ml-2 mr-2"><div class="a">あなたの肌に一撃を</div></h1>
-        </div>
+            <h1 class="text-lg font-bold text-yellow-600 ml-2 mr-2">
+                <div class="a">あなたの肌に一撃を</div>
+            </h1>
+        </a>
+    </div>
 
-        <!-- 検索バー -->
-                    <form action="API.php" method="GET" class="flex items-center w-2/3 mt-0 ml-2 mr-2">
-                <input 
-                    type="search"
-                    name="query"
-                    placeholder="ブランド名、カテゴリ名、商品名で探す" 
-                    class="w-full p-2 border border-4 border-gray-200 rounded-l-lg focus:outline-none"
-                    value="<?php echo isset($_GET['query']) ? htmlspecialchars($_GET['query'], ENT_QUOTES) : ''; ?>">
-                
-                <button 
-                    type="submit" 
-                    class="p-2 border border-4  bg-gray-200 text-black rounded-r-lg hover:bg-gray-200 focus:outline-none"
-                >
-                <div class="dli-search"></div>
-
-                </button>
-            </form>
+    <!-- 検索バー -->
+    <form action="API.php" method="GET" class="flex items-center w-2/3 mt-0 ml-2 mr-2">
+        <input 
+            type="search"
+            name="query"
+            placeholder="ブランド名、カテゴリ名、商品名で探す" 
+            class="w-full p-2 border border-4 border-gray-200 rounded-l-lg focus:outline-none"
+            value="<?php echo isset($_GET['query']) ? htmlspecialchars($_GET['query'], ENT_QUOTES) : ''; ?>">
         
+        <button 
+            type="submit" 
+            class="p-2 border border-4 bg-gray-200 text-black rounded-r-lg hover:bg-gray-200 focus:outline-none"
+        >
+            <div class="dli-search"></div>
+        </button>
+    </form>
+</header>
+
          <!-- ハンバーガーメニュー -->
         <button id="menuButton" class="text-3xl focus:outline-none ml-4 mr-4">
             <i class="fas fa-bars"></i>
